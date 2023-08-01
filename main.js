10da12d8e184869f992d5c4f50bc44519e1bea8f
Vu Anh Lam <vuanhlam000@gmail.com>
2023-08-01 17:22:59 +0700

    successfully draw

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ import OLGoogleMaps from "olgm/OLGoogleMaps.js";
 import GoogleLayer from "olgm/layer/Google.js";
 import { defaults as defaultInteractions } from "olgm/interaction.js";
 import { transform } from "ol/proj.js";
-
-const center = [107.84641987555096, 15.557018118480753];
-// const center = [15.557018118480753, 107.84641987555096];
+import { OSM, Vector as VectorSource } from "ol/source.js";
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
+import { Draw, Modify, Snap } from "ol/interaction.js";
 
 // This dummy layer tells Google Maps to switch to its default map type
 // const googleLayer = new GoogleLayer();
@@ -15,19 +15,59 @@ const SatelliteLayer = new GoogleLayer({
   mapTypeId: google.maps.MapTypeId.SATELLITE,
 });
 
+const source = new VectorSource();
+const vector = new VectorLayer({
+  source: source,
+  style: {
+    "fill-color": "rgba(255, 255, 255, 0.2)",
+    "stroke-color": "#ffcc33",
+    "stroke-width": 2,
+    "circle-radius": 7,
+    "circle-fill-color": "#ffcc33",
+  },
+});
+
 const map = new Map({
   // use OL3-Google-Maps recommended default interactions
   interactions: defaultInteractions(),
-  layers: [
-    // googleLayer,
-    SatelliteLayer,
-  ],
+  layers: [SatelliteLayer, vector],
   target: "map",
   view: new View({
-    center: transform([107.84641987555096, 15.557018118480753], "EPSG:4326", "EPSG:3857"),
+    center: transform(
+      [107.84641987555096, 15.557018118480753],
+      "EPSG:4326",
+      "EPSG:3857"
+    ),
     zoom: 5.8,
   }),
 });
+
+const modify = new Modify({ source: source });
+map.addInteraction(modify);
+
+let draw, snap; // global so we can remove them later
+const typeSelect = document.getElementById("type");
+
+function addInteractions() {
+  draw = new Draw({
+    source: source,
+    type: typeSelect.value,
+  });
+  map.addInteraction(draw);
+  snap = new Snap({ source: source });
+  map.addInteraction(snap);
+}
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function () {
+  map.removeInteraction(draw);
+  map.removeInteraction(snap);
+  addInteractions();
+};
+
+addInteractions();
 
 const olGM = new OLGoogleMaps({ map: map }); // map is the Map instance
 olGM.activate();
