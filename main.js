01bf4a6ae93039c2c570689fced0ba0988fc1caf
Vu Anh Lam <vuanhlam000@gmail.com>
2023-08-03 08:51:53 +0700

    measure

--- main.js
+++ main.js
@@ -7,19 +7,98 @@ import { transform } from "ol/proj.js";
 import { OSM, Vector as VectorSource } from "ol/source.js";
 import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
 import { Draw, Modify, Snap } from "ol/interaction.js";
-import {toLonLat} from 'ol/proj';
+import { toLonLat } from "ol/proj";
+import Overlay from "ol/Overlay.js";
+import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style.js";
+import { unByKey } from "ol/Observable.js";
+import { LineString, Polygon } from "ol/geom.js";
+import { getArea, getLength } from "ol/sphere.js";
+
+let result = {
+  longitude: "",
+  latitude: "",
+  km2: "",
+  km: "",
+};
 
 /**
- *! Layer 
-*/
+ * Currently drawn feature.
+ * @type {import("../src/ol/Feature.js").default}
+ */
+let sketch;
+
+/**
+ * The help tooltip element.
+ * @type {HTMLElement}
+ */
+let helpTooltipElement;
+
+/**
+ * Overlay to show the help messages.
+ * @type {Overlay}
+ */
+let helpTooltip;
+
+/**
+ * The measure tooltip element.
+ * @type {HTMLElement}
+ */
+let measureTooltipElement;
+
+/**
+ * Overlay to show the measurement.
+ * @type {Overlay}
+ */
+let measureTooltip;
+
+/**
+ * Message to show when the user is drawing a polygon.
+ * @type {string}
+ */
+const continuePolygonMsg = "Click to continue drawing the polygon";
+
+/**
+ * Message to show when the user is drawing a line.
+ * @type {string}
+ */
+const continueLineMsg = "Click to continue drawing the line";
+
+/**
+ * Handle pointer move.
+ * @param {import("../src/ol/MapBrowserEvent").default} evt The event.
+ */
+const pointerMoveHandler = function (evt) {
+  if (evt.dragging) {
+    return;
+  }
+  /** @type {string} */
+  let helpMsg = "Click to start drawing";
+
+  if (sketch) {
+    const geom = sketch.getGeometry();
+    if (geom instanceof Polygon) {
+      helpMsg = continuePolygonMsg;
+    } else if (geom instanceof LineString) {
+      helpMsg = continueLineMsg;
+    }
+  }
+
+  helpTooltipElement.innerHTML = helpMsg;
+  helpTooltip.setPosition(evt.coordinate);
+
+  helpTooltipElement.classList.remove("hidden");
+};
+
+/**
+ *! Layer
+ */
 const SatelliteLayer = new GoogleLayer({
   mapTypeId: google.maps.MapTypeId.SATELLITE,
 });
 
-
 /**
- *! Vector 
-*/
+ *! Vector
+ */
 const source = new VectorSource();
 const vector = new VectorLayer({
   source: source,
@@ -50,38 +129,82 @@ const map = new Map({
 /**
  *! interaction used for modifying vector features on the map
  *! It allows users to edit the vertices and geometry of vector features, such as points, lines, and polygons.
- *! edit when drawed vector 
-*/
+ *! edit when drawed vector
+ */
 const modify = new Modify({ source: source });
 map.addInteraction(modify);
 
 let draw, snap; // global so we can remove them later
 const typeSelect = document.getElementById("type");
-console.log(typeSelect.value);
 
 function addInteractions() {
   draw = new Draw({
     source: source,
     type: typeSelect.value,
+    style: new Style({
+      fill: new Fill({
+        color: "rgba(255, 255, 255, 0.2)",
+      }),
+      stroke: new Stroke({
+        color: "red",
+        lineDash: [10, 10],
+        width: 2,
+      }),
+      //* circle follow the mouse
+      image: new CircleStyle({
+        radius: 5,
+        stroke: new Stroke({
+          color: "rgba(0, 0, 0, 0.7)",
+        }),
+        fill: new Fill({
+          color: "yellow",
+        }),
+      }),
+    }),
   });
-  // console.log(draw);
   map.addInteraction(draw);
-  console.log(typeSelect.value);
-  snap = new Snap({ source: source });
-  map.addInteraction(snap);
+
+  createMeasureTooltip();
+  createHelpTooltip();
+
+  let listener;
+  draw.on("drawstart", function (evt) {
+    // set sketch
+    sketch = evt.feature;
+
+    /** @type {import("../src/ol/coordinate.js").Coordinate|undefined} */
+    let tooltipCoord = evt.coordinate;
+
+    listener = sketch.getGeometry().on("change", function (evt) {
+      const geom = evt.target;
+      let output;
+      if (geom instanceof Polygon) {
+        output = formatArea(geom);
+        tooltipCoord = geom.getInteriorPoint().getCoordinates();
+      } else if (geom instanceof LineString) {
+        output = formatLength(geom);
+        tooltipCoord = geom.getLastCoordinate();
+      }
+      measureTooltipElement.innerHTML = output;
+      // console.log(output);
+      measureTooltip.setPosition(tooltipCoord);
+    });
+  });
+
+  draw.on("drawend", function () {
+    measureTooltipElement.className = "ol-tooltip ol-tooltip-static";
+    measureTooltip.setOffset([0, -7]);
+    // unset sketch
+    sketch = null;
+    // unset tooltip so that a new one can be created
+    measureTooltipElement = null;
+    createMeasureTooltip();
+    unByKey(listener);
+  });
 }
 
-/**
- * Handle change event.
- */
-typeSelect.onchange = function () {
-  map.removeInteraction(draw); //* remove type of drawing when switch to the new one 
-  map.removeInteraction(snap);
-  addInteractions();
-};
-
 //TODO: Get coordinate when point a place on map
-map.on('click', function(event) { 
+map.on("click", function (event) {
   // Get the clicked coordinate from the event
   const clickedCoordinate = event.coordinate;
 
@@ -93,10 +216,104 @@ map.on('click', function(event) {
   const longitude = lonLatCoordinate[0];
 
   // Now you have the latitude and longitude of the clicked point
-  console.log('Vĩ độ -> Latitude:', latitude, 'Kinh độ -> Longitude:', longitude);
+  console.log(
+    "Vĩ độ -> Latitude:",
+    latitude,
+    "Kinh độ -> Longitude:",
+    longitude
+  );
 });
+
+// --------------------------------------------- Get length -----------------------------------------//
+map.on("pointermove", pointerMoveHandler);
+
+map.getViewport().addEventListener("mouseout", function () {
+  helpTooltipElement.classList.add("hidden");
+});
+
+/**
+ * Format length output.
+ * @param {LineString} line The line.
+ * @return {string} The formatted length.
+ */
+const formatLength = function (line) {
+  // console.log(line);
+  const length = getLength(line);
+  let output;
+  if (length > 100) {
+    output = Math.round((length / 1000) * 100) / 100 + " " + "km";
+  } else {
+    output = Math.round(length * 100) / 100 + " " + "m";
+  }
+  return output;
+};
+
+/**
+ * Format area output.
+ * @param {Polygon} polygon The polygon.
+ * @return {string} Formatted area.
+ */
+const formatArea = function (polygon) {
+  const area = getArea(polygon);
+  let output;
+  if (area > 10000) {
+    output = Math.round((area / 1000000) * 100) / 100 + " " + "km<sup>2</sup>";
+  } else {
+    output = Math.round(area * 100) / 100 + " " + "m<sup>2</sup>";
+  }
+  return output;
+};
+
+/**
+ * Creates a new help tooltip
+ */
+function createHelpTooltip() {
+  if (helpTooltipElement) {
+    helpTooltipElement.parentNode.removeChild(helpTooltipElement);
+  }
+  helpTooltipElement = document.createElement("div");
+  helpTooltipElement.className = "ol-tooltip hidden";
+  helpTooltip = new Overlay({
+    element: helpTooltipElement,
+    offset: [15, 0],
+    positioning: "center-left",
+  });
+  map.addOverlay(helpTooltip);
+}
+
+/**
+ * Creates a new measure tooltip
+ */
+function createMeasureTooltip() {
+  if (measureTooltipElement) {
+    measureTooltipElement.parentNode.removeChild(measureTooltipElement);
+  }
+  measureTooltipElement = document.createElement("div");
+  measureTooltipElement.className = "ol-tooltip ol-tooltip-measure";
+  measureTooltip = new Overlay({
+    element: measureTooltipElement,
+    offset: [0, -15],
+    positioning: "bottom-center",
+    stopEvent: false,
+    insertFirst: false,
+  });
+  map.addOverlay(measureTooltip);
+}
+
+/**
+ *! remove type of drawing when switch to the new one
+ */
+typeSelect.onchange = function () {
+  map.removeInteraction(draw);
+  map.removeInteraction(snap);
+  addInteractions();
+};
 
 addInteractions();
 
 const olGM = new OLGoogleMaps({ map: map }); // map is the Map instance
 olGM.activate();
+
+function myFunction() {
+  console.log('ahihi');
+}
